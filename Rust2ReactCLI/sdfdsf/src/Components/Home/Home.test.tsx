7ddcdee5769cc5801487de606b4b724b15f6c305
flodhest <eric.flodin@aol.com>
2024-01-22 09:40:40 +0100

    Added Jest testing functionality

--- Rust2ReactCLI/sdfdsf/src/Components/Home/Home.test.tsx
+++
@@ -1,9 +0,0 @@
-//Home.test.txt
-
-use super::*;
-
-#[wasm_bindgen_test]
-fn test_home() {
-    let home = Home::new();
-    assert_eq!(home.render().to_string(), "Home component content");
-}
--- Rust2ReactCLI/sdfdsf/src/Utils/utils.tsx
+++
@@ -1,8 +0,0 @@
-// Utils.tsx
-
-// This is a placeholder file for your Utils.
-// Feel free to replace the content based on your requirements.
-
-export const exampleUtil = () => {
-  // Your utility function implementation goes here
-};  
